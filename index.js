d474f3d391c12ed0abbd7f22153a110ad6e6d399
j <137029997+Jagacool@users.noreply.github.com>
2023-09-21 21:41:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,151 +1,65 @@
-const express = require("express");
-const app = express();
-const mongodb = require("mongodb");
-const mongoclient = mongodb.MongoClient;
-const dotenv = require("dotenv").config();
-const URL =process.env.DB;
+import { mentors } from "./routes/mentor.js";
+import { students } from "./routes/student.js";
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import {MongoClient} from "mongodb"; 
+
+dotenv.config();
+
+export const app = express();
+//vivek
+const port=process.env.PORT;
+
+const mentor=[{
+"id":1,
+"mentor_name":"vivek",
+"skill":"react.js"
+},
+{
+  "id":2,
+  "mentor_name":"balaji",
+  "skill":"angular.js"
+},
+{
+"id":3,
+"mentor_name":"gopal",
+"skill":"vue.js"  
+}];
+
+const student=[{
+  "id":101,
+"student_name":"sankar",
+"student_email":"vivek@gmail.com",
+"mentor_id":"1"
+},{"id":102,
+"student_name":"surya",
+"student_email":"vivek@gmail.com",
+"mentor_id":""
+}];
+
+//const MONGO_URL="mongodb://localhost";//if port is defalut
+
+const MONGO_URL=process.env.MONGO_URL;
+app.use(cors())
+async function createConnection(){
+const client=new MongoClient(MONGO_URL);
+await client.connect();
+console.log("DB Connected");
+return client;
+}
+
+export const client=await createConnection();
+
 app.use(express.json());
-let mentor = [];
-let student = [];
 
- 
-  app.get('/', (req, res) => {
-    res.status(200).json({
-      message: 'Welcome to Mentor and Student assigning with database'
-    });
-  });
-
-app.post("/create_mentor", async (req, res) => {
-  try {
-    const connection = await mongoclient.connect(URL);
-    const db = connection.db("assignment");
-    const mentor = await db.collection("mentors").insertOne(req.body);
-    await connection.close();
-    res.json({ message: "Mentor created", id: mentor.insertedId });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
+app.get('/', (req, res) => {
+  res.send('Hello World!!!!😊😊😊🔥🔥🔥🔥')
 });
 
-app.post("/create_student", async (req, res) => {
-  try {
-    const connection = await mongoclient.connect(URL);
-    const db = connection.db("assignment");
-    const student = await db.collection("students").insertOne(req.body);
-    await connection.close();
-    res.json({ message: "Student created", id: student.insertedId });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
+app.use("/mentors",mentors);
+app.use("/students",students);
 
-/** get all mentors */
-app.get("/mentors", async (req, res) => {
-  try {
-    const connection = await mongoclient.connect(URL);
-    const db = connection.db("assignment");
-    const mentor = await db.collection("mentors").find({}).toArray();
-    await connection.close();
-    res.json(mentor);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
-
-/** get student */
-
-app.get("/students", async (req, res) => {
-  try {
-    const connection = await mongoclient.connect(URL);
-    const db = connection.db("assignment");
-    const student = await db.collection("students").find({}).toArray();
-    await connection.close();
-    res.json(student);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
-
-/** assign student to a mentor */
-app.put("/assign_student/:id", async (req, res) => {
-  try {
-    const connection = await mongoclient.connect(URL);
-    const db = connection.db("assignment");
-    const mentordata = await db
-      .collection("mentors")
-      .findOne({ _id: mongodb.ObjectId(req.params.id) });
-    if (mentordata) {
-      delete req.body._id;
-      const mentor = await db
-        .collection("mentors")
-        .updateOne(
-          { _id: mongodb.ObjectId(req.params.id) },
-          { $set: req.body }
-        );
-      await connection.close();
-
-      res.json(mentor);
-    } else {
-      res.status(404).json({ message: "mentor not found" });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
-
-/** show all students of particular mentor */
-
-app.get("/mentor_student/:id", async (req, res) => {
-  try {
-    const connection = await mongoclient.connect(URL);
-    const db = connection.db("assignment");
-    const mentor = await db
-      .collection("mentors")
-      .findOne({ _id: mongodb.ObjectId(req.params.id) });
-    await connection.close();
-    if (mentor) {
-      res.json(`students name : ${mentor.student} assigned to ${mentor.name}`);
-    } else {
-      res.status(404).json({ message: "Mentor not found" });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Something went wrong" });
-  }
-});
-/** assign or change mentor for student */
-app.put("/assign_change_mentor/:id", async(req, res) => {
-    try {
-        const connection = await mongoclient.connect(URL);
-        const db = connection.db("assignment");
-        const studentdata = await db
-        .collection("students")
-        .findOne({ _id: mongodb.ObjectId(req.params.id) });
-        if (studentdata) {
-            delete req.body._id;
-            const student = await db
-              .collection("students")
-              .updateOne(
-                { _id: mongodb.ObjectId(req.params.id) },
-                { $set: req.body }
-              );
-            await connection.close();
-      
-            res.json(student);
-          } else {
-            res.status(404).json({ message: "Student not found" });
-          }
-        await connection.close();
-        res.json(student);
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Something went wrong" });
-      }
-});
-
-app.listen(process.env.PORT || 3003);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
