9e792a1f46fac23e948c13cb5164b0aaf5e45059
j <137029997+Jagacool@users.noreply.github.com>
2023-09-21 11:16:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,161 +1,151 @@
 const express = require("express");
-const mongodb = require("mongodb");
-var cors = require('cors')
-require("dotenv").config();
-const mongoClient = mongodb.MongoClient;
 const app = express();
-const port = 3000;
-const dbURL = process.env.DB_URL;
+const mongodb = require("mongodb");
+const mongoclient = mongodb.MongoClient;
+const dotenv = require("dotenv").config();
+const URL =process.env.DB;
 app.use(express.json());
-app.use(cors());
+let mentor = [];
+let student = [];
 
-//create a mentor
-
-app.post("/create-mentor", async (req, res) => {
-  try {
-    let client = await mongoClient.connect(dbURL);
-    let db = client.db("mentor_assign_db");
-    let result = await db.collection("mentors").insertOne(req.body);
-    res.status(200).json({ message: "mentor created" });
-    client.close();
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
-// create a student
-app.post("/create-student", async (req, res) => {
-  try {
-    let client = await mongoClient.connect(dbURL);
-    let db = client.db("mentor_assign_db");
-    let result = db.collection("students").insertOne(req.body);
-    res.status(200).json({ message: "students created" });
-    client.close();
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
-
-// assign a mentor
-
-app.put("/assign-mentor", async (req, res) => {
-  try {
-      let client = await mongoClient.connect(dbURL);
-      console.log(req.body.student_name)
-      let db = client.db("mentor_assign_db");
-      await db
-          .collection("students")
-          .updateMany(
-              { student_name:  req.body.student_name },
-              { $set: { mentor_name: req.body.mentor_name } }
-          );    
-      let result = await db
-          .collection("students")
-          .find( )
-          .toArray();
-      res.status(200).json({ message: "mentor assigned", result });
-      client.close();
-  } catch (error) {
-      console.log(error);
-      res.sendStatus(500);
-  }
-});
-
-// assign a student 
-
-app.put("/assign-student", async (req, res) => {
-  try {
-      let client = await mongoClient.connect(dbURL);
-      let db = client.db("mentor_assign_db");
-      let result = await db
-          .collection("students")
-          .updateMany(
-              {mentor_name: req.body.mentor_name },
-              { $set: { student_name:  req.body.student_name } }
-          );
-      res.status(200).json({
-          message: "mentor assigned or changed to a particular student"
-      });
-      client.close();
-  } catch (error) {
-      console.log(error);
-      res.sendStatus(500);
-  }
-});
-// read students list
-
-app.get("/students-list", async (req, res) => {
-  try {
-    let client = await mongoClient.connect(dbURL);
-    let db = client.db("mentor_assign_db");
-    let result = await db.collection("students").find().toArray();
-    res.status(200).json({ message: " student list", result });
-    client.close();
-    console.log("atlas")
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(500);
-  }
-});
-
-// read mentors list
-
-app.get("/mentors-list", async (req, res) => {
-    try {
-      let client = await mongoClient.connect(dbURL);
-      let db = client.db("mentor_assign_db");
-      let result = await db.collection("mentors").find().toArray();
-      res.status(200).json({ message: " mentors list", result });
-      client.close();
-    } catch (error) {
-      console.log(error);
-      res.sendStatus(500);
-    }
+ 
+  app.get('/', (req, res) => {
+    res.status(200).json({
+      message: 'Welcome to Mentor and Student assigning with database'
+    });
   });
 
-
-  // students without mentor
-  app.get("/idle-students", async (req, res) => {
-    try {
-        let client = await mongoClient.connect(dbURL);
-        let db = client.db("mentor_assign_db");
-        let result = await db
-            .collection("students")
-            .find({ mentor_name: null })
-            .toArray();
-        res.status(200).json({
-            message: "list of students without a mentor",
-            result,
-        });
-        client.close();
-    } catch (error) {
-        console.log(error);
-        res.sendStatus(500);
-    }
-});
-
-// students under mentor
-app.get("/students-under-mentor/:mentor_name", async (req, res) => {
+app.post("/create_mentor", async (req, res) => {
   try {
-      let client = await mongoClient.connect(dbURL);
-      let db = client.db("mentor_assign_db");
-
-      let result = await db
-          .collection("students")
-          .find({ mentor_name: "mentor 1" })
-          .toArray();
-      res.status(200).json({
-          message: `students under mentor_name: `,
-          result,  
-      });
-      client.close();
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("assignment");
+    const mentor = await db.collection("mentors").insertOne(req.body);
+    await connection.close();
+    res.json({ message: "Mentor created", id: mentor.insertedId });
   } catch (error) {
-      console.log(error);
-      res.sendStatus(500);
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 
-app.listen(port, () => {
-  console.log("server started at " + port);
+app.post("/create_student", async (req, res) => {
+  try {
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("assignment");
+    const student = await db.collection("students").insertOne(req.body);
+    await connection.close();
+    res.json({ message: "Student created", id: student.insertedId });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
+
+/** get all mentors */
+app.get("/mentors", async (req, res) => {
+  try {
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("assignment");
+    const mentor = await db.collection("mentors").find({}).toArray();
+    await connection.close();
+    res.json(mentor);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+/** get student */
+
+app.get("/students", async (req, res) => {
+  try {
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("assignment");
+    const student = await db.collection("students").find({}).toArray();
+    await connection.close();
+    res.json(student);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+/** assign student to a mentor */
+app.put("/assign_student/:id", async (req, res) => {
+  try {
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("assignment");
+    const mentordata = await db
+      .collection("mentors")
+      .findOne({ _id: mongodb.ObjectId(req.params.id) });
+    if (mentordata) {
+      delete req.body._id;
+      const mentor = await db
+        .collection("mentors")
+        .updateOne(
+          { _id: mongodb.ObjectId(req.params.id) },
+          { $set: req.body }
+        );
+      await connection.close();
+
+      res.json(mentor);
+    } else {
+      res.status(404).json({ message: "mentor not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+/** show all students of particular mentor */
+
+app.get("/mentor_student/:id", async (req, res) => {
+  try {
+    const connection = await mongoclient.connect(URL);
+    const db = connection.db("assignment");
+    const mentor = await db
+      .collection("mentors")
+      .findOne({ _id: mongodb.ObjectId(req.params.id) });
+    await connection.close();
+    if (mentor) {
+      res.json(`students name : ${mentor.student} assigned to ${mentor.name}`);
+    } else {
+      res.status(404).json({ message: "Mentor not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+/** assign or change mentor for student */
+app.put("/assign_change_mentor/:id", async(req, res) => {
+    try {
+        const connection = await mongoclient.connect(URL);
+        const db = connection.db("assignment");
+        const studentdata = await db
+        .collection("students")
+        .findOne({ _id: mongodb.ObjectId(req.params.id) });
+        if (studentdata) {
+            delete req.body._id;
+            const student = await db
+              .collection("students")
+              .updateOne(
+                { _id: mongodb.ObjectId(req.params.id) },
+                { $set: req.body }
+              );
+            await connection.close();
+      
+            res.json(student);
+          } else {
+            res.status(404).json({ message: "Student not found" });
+          }
+        await connection.close();
+        res.json(student);
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong" });
+      }
+});
+
+app.listen(process.env.PORT || 3003);
